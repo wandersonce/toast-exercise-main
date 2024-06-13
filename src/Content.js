506d47475ea44dde5215e0b62c4bb430376b41fc
Wanderson Castro <wandersonce1@hotmail.com>
2024-06-13 13:18:23 -0700

    Adding table and handling the data inside it to display correctly

--- src/Content.js
+++ src/Content.js
@@ -1,17 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import { onMessage, saveLikedFormSubmission } from './service/mockServer';
-import { IconButton, Snackbar, SnackbarContent } from '@mui/material';
+import {
+  fetchLikedFormSubmissions,
+  onMessage,
+  saveLikedFormSubmission,
+} from './service/mockServer';
+import {
+  IconButton,
+  Paper,
+  Snackbar,
+  SnackbarContent,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
 import { Close, ThumbUp } from '@mui/icons-material';
 
 export default function Content() {
   //Setting states to handle data
   const [toastStatus, setToastStatus] = useState(false); //False - Closed ; True - Open
   const [submissionData, setSubmissionData] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [tableRows, setTableRows] = useState([]);
+  const [likedData, setLikedData] = useState([]);
 
   const handleSubmission = (submittedData) => {
-    setSubmissionData(submittedData.data);
+    setSubmissionData(submittedData);
     if (submittedData.data) {
       setToastStatus(true);
     }
@@ -22,6 +40,21 @@ export default function Content() {
       .then((res) => {
         console.log(res);
         setToastStatus(false);
+        setIsLoading(true);
+        getLikedToasts();
+        setIsLoading(false);
+        return;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const getLikedToasts = () => {
+    setIsLoading(true);
+    fetchLikedFormSubmissions()
+      .then((res) => {
+        setLikedData(res.formSubmissions);
       })
       .catch((err) => {
         console.log(err);
@@ -29,16 +62,36 @@ export default function Content() {
   };
 
   onMessage(handleSubmission);
-  useEffect(() => {}, []);
+  useEffect(() => {
+    setIsLoading(true);
+    getLikedToasts();
+    setIsLoading(false);
+  }, []);
   return (
     <>
       <Box sx={{ marginTop: 3 }}>
         <Typography variant="h4">Liked Form Submissions</Typography>
-
-        <Typography
-          variant="body1"
-          sx={{ fontStyle: 'italic', marginTop: 1 }}
-        ></Typography>
+        <TableContainer component={Paper} sx={{ maxWidth: '650px' }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell align="left">Name</TableCell>
+                <TableCell align="left">Email</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {likedData.length > 0 &&
+                likedData.map((item) => (
+                  <TableRow key={item.id}>
+                    <TableCell>
+                      {item.data.firstName} {item.data.lastName}
+                    </TableCell>
+                    <TableCell>{item.data.email}</TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Box>
       <Snackbar
         open={toastStatus}
@@ -48,7 +101,7 @@ export default function Content() {
         <SnackbarContent
           sx={{ background: '#1976D2', width: '100%' }}
           message={
-            submissionData && (
+            submissionData.data && (
               <Box
                 sx={{
                   color: '#FFFFFF',
@@ -60,10 +113,11 @@ export default function Content() {
               >
                 <Box sx={{ display: 'flex', flexDirection: 'column' }}>
                   <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
-                    {submissionData.firstName} {submissionData.lastName}
+                    {submissionData.data.firstName}{' '}
+                    {submissionData.data.lastName}
                   </Typography>
                   <Typography sx={{ fontStyle: 'italic' }}>
-                    {submissionData.email}
+                    {submissionData.data.email}
                   </Typography>
                 </Box>
                 <IconButton
