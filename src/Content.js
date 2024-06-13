2b46a8f6a547ae3d14e918fc86cac818751de828
Wanderson Castro <wandersonce1@hotmail.com>
2024-06-13 14:13:02 -0700

    Add error state handling and Content table

--- src/Content.js
+++ src/Content.js
@@ -7,6 +7,7 @@ import {
   saveLikedFormSubmission,
 } from './service/mockServer';
 import {
+  CircularProgress,
   IconButton,
   Paper,
   Snackbar,
@@ -21,12 +22,16 @@ import {
 import { Close, ThumbUp } from '@mui/icons-material';
 
 export default function Content() {
+  //** WITH MORE TIME I WOULD DO SOME GLOBAL STATES TO HANDLE SOME STATUS HERE **//
+  //** I would use React Context or Zustand **//
+
   //Setting states to handle data
   const [toastStatus, setToastStatus] = useState(false); //False - Closed ; True - Open
   const [submissionData, setSubmissionData] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
-  const [tableRows, setTableRows] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [likedData, setLikedData] = useState([]);
+  const [refetchAttempts, setRefetchAttempts] = useState(0);
+  const [error, setError] = useState('as');
 
   const handleSubmission = (submittedData) => {
     setSubmissionData(submittedData);
@@ -38,7 +43,6 @@ export default function Content() {
   const handleLikeToast = () => {
     saveLikedFormSubmission(submissionData)
       .then((res) => {
-        console.log(res);
         setToastStatus(false);
         setIsLoading(true);
         getLikedToasts();
@@ -51,37 +55,71 @@ export default function Content() {
   };
 
   const getLikedToasts = () => {
-    setIsLoading(true);
     fetchLikedFormSubmissions()
       .then((res) => {
         setLikedData(res.formSubmissions);
       })
       .catch((err) => {
         console.log(err);
+
+        //Retrying to get data from server every 3s ; It will try 5x
+        setTimeout(() => {
+          setRefetchAttempts(refetchAttempts + 1);
+          if (refetchAttempts < 6) {
+            getLikedToasts();
+          } else {
+            setError(
+              'Sorry we were not able to get the data at this time. Please try again later'
+            );
+            setRefetchAttempts(0);
+          }
+        }, 3000);
+        setIsLoading(false);
+      })
+      .finally(() => {
+        setIsLoading(false);
+        setRefetchAttempts(0);
       });
   };
 
   onMessage(handleSubmission);
   useEffect(() => {
-    setIsLoading(true);
     getLikedToasts();
-    setIsLoading(false);
   }, []);
+
+  console.log(isLoading, likedData.length, error);
   return (
     <>
       <Box sx={{ marginTop: 3 }}>
         <Typography variant="h4">Liked Form Submissions</Typography>
-        <TableContainer component={Paper} sx={{ maxWidth: '650px' }}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell align="left">Name</TableCell>
-                <TableCell align="left">Email</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {likedData.length > 0 &&
-                likedData.map((item) => (
+        {!isLoading && error !== '' && likedData.length == 0 && (
+          <Typography variant="p">{error}</Typography>
+        )}
+        {isLoading && error !== '' && (
+          <Box
+            sx={{
+              marginTop: '30px',
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+          >
+            <CircularProgress />
+          </Box>
+        )}
+        {!isLoading && likedData.length > 0 && error !== '' && (
+          <TableContainer
+            component={Paper}
+            sx={{ maxWidth: '650px', marginTop: '30px' }}
+          >
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell align="left">Name</TableCell>
+                  <TableCell align="left">Email</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {likedData.map((item) => (
                   <TableRow key={item.id}>
                     <TableCell>
                       {item.data.firstName} {item.data.lastName}
@@ -89,9 +127,10 @@ export default function Content() {
                     <TableCell>{item.data.email}</TableCell>
                   </TableRow>
                 ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        )}
       </Box>
       <Snackbar
         open={toastStatus}
